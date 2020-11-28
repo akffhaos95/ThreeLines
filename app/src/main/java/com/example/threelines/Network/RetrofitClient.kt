905884efa2644988da073db130d709daa773c964
akffhaos95 <akffhaos95@naver.com>
2020-11-28 12:14:17 +0900

    Record Time

--- app/src/main/java/com/example/threelines/Network/RetrofitClient.kt
+++ app/src/main/java/com/example/threelines/Network/RetrofitClient.kt
@@ -8,7 +8,7 @@ import java.util.concurrent.TimeUnit
 
 object RetrofitClient {
     private var retrofit: Retrofit? = null
-    private const val BASEURL = "https://326593b3d3d1.ngrok.io/"
+    private const val BASEURL = "https://2cfb5d1d3ffc.ngrok.io/"
 
     fun getRetrofit(): Retrofit {
         val gson = GsonBuilder()
